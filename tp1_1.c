f1145757ec7e15655b29b0070327269671d15c02
Divitrion <85127062+Divitrion@users.noreply.github.com>
2022-04-04 20:58:17 -0300

    TP01

---
+++ tp1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("Hola Mundo \n");
+      int *puntero, variable=420;
+
+    puntero = &variable;
+
+    printf("Contenido del puntero: %d \n",*puntero);
+    printf("Direccion de memoria almacenada: %p \n",puntero);
+    printf("Direccion de memoria de la variable: %p \n",&variable);
+    printf("Direccion de memoria del puntero: %p \n",&puntero);
+    printf("Tamaño de la variable: %d \n",sizeof(variable));
+return 0;
+}
---
+++ tp1_2.c
@@ -0,0 +1,60 @@
+#include <math.h>
+#include <stdio.h>
+
+int cuadrado(int numero);
+void cuadradovoid(int *numero);
+void invertir(int *c, int *b);
+void orden(int *c, int *b);
+
+int main(){
+    int numero, *pnumero, *pa, *pb, a, b;
+
+    puts("Ingrese un numero");
+    scanf("%d",&numero);
+    pnumero=&numero;
+    printf("contenido de la variable: %d \n",*pnumero);
+    printf("direccion de la variable: %p \n",pnumero);
+    printf("el cuadrado del numero es: %d \n",cuadrado(*pnumero));
+    cuadradovoid(pnumero);
+    puts("-------------------------");
+    puts("Ingrese dos numeros");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    pa=&a;
+    pb=&b;
+    printf("La variable a es: %d\n",a);
+    printf("La variable b es: %d\n",b);
+    invertir(pa,pb);
+    printf("La variable a ahora es: %d\n",a);
+    printf("La variable b ahora es: %d\n",b);
+    orden(pa,pb);
+    printf ("El valor mas chico es: %d\n",a);
+    printf ("El valor mas grande es: %d\n",b);
+return 0;
+}
+
+int cuadrado(int numero){
+    numero=pow(numero,2);
+    return(numero);
+}
+
+void cuadradovoid(int *numero){
+    *numero=pow(*numero,2);
+    printf("Cuadrado desde una funcion void: %d\n",*numero);
+}
+
+void invertir(int *a, int *b){
+    int aux;
+    aux= *a;
+    *a=*b;
+    *b=aux;
+}
+
+void orden(int *a, int *b){
+    int aux;
+    if(*a>*b){
+        aux = *a;
+        *a = *b;
+        *b = aux;
+    }
+}
